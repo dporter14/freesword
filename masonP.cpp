2670c8c5df5029bbc17f4c47774612ab16be3f1d
Mason Pierce <mpierce.cs.csubak.edu>
2018-02-23 13:14:20 -0800

    More classes

--- masonP.cpp
+++ masonP.cpp
@@ -3,10 +3,23 @@
 #include "defs.h"
 class Menu
 {
+	private:
+	float height, width;
+	
+	
 	public:
 	Menu()
 	{
+	
 	}
+};
+class mainMenu : public menu
+{
+
+};
+class ammo : public menu
+{
+
 };
 int mason_func()
 {	
