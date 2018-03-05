196dc42001547e2dd64e075d20158aafb6f45c9e
Mason Pierce <mpierce.cs.csubak.edu>
2018-03-04 20:56:23 -0800

    Work on pause.

--- masonP.cpp
+++ masonP.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-
+#include "GL/glx.h"
+#include "fonts.h"
 #include "global.h"
 
+
+void mason_func()
+{
+	strcpy(g.title.text,"Mason: Test test");
+	g.title.text_color = 0x00ff00ff;
+}
 
 Menu::Menu() 
 {
@@ -10,14 +17,25 @@ Menu::Menu()
 	m_buttonTitle = "";
 }
 
-bool pause() {
-	bool paused = false;
-	
-	
-}
-	
-void mason_func()
+bool pause() 
 {
-	strcpy(g.title.text,"Mason: Test test");
-	g.title.text_color = 0x00ff00ff;
+	bool paused = true;
+	Rect rects [2];
+
+
+	static float angle = 0.0;
+	glColor3ub(0, 255, 0);
+	glPushMatrix();
+	glTranslatef(x, y, 0.0);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	glTranslatef(-50.0, -50.0, 0);
+	glBegin(GL_QUADS);
+		glVertex2i(0,     0);
+		glVertex2i(0,   200);
+		glVertex2i(200, 100);
+		glVertex2i(200,   0);
+	glEnd();
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPopMatrix();	
 }
