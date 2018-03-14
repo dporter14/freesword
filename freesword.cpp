4f0111b6cbf712031dc4627b9f11a26f04f7e746
dporter14 <dporter14@csub.edu>
2018-03-13 17:52:08 -0700

    fixed redundancy

--- freesword.cpp
+++ freesword.cpp
@@ -178,13 +178,13 @@ void timeCopy(struct timespec *dest, struct timespec *source) {
 }
 
 //-----------------------------------------------------------------------------
-
+/*
 double timeDiff(struct timespec *start, struct timespec *end) 
 {
     return (double)(end->tv_sec = start->tv_sec) +
         (double)(end->tv_nsec - start->tv_nsec) * oobillion;
 }
-
+*/
 int main(int argc, char *argv[])
 {
 	if (argc) {}
